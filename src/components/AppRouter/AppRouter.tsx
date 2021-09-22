c23f051071e7edf067a2b23fe87c351ce23390bf
kannadurdiev <kolyabek13@gmail.com>
2021-09-22 12:54:27 +0500

    setted api url

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react'
 import { Redirect, Route, Switch } from 'react-router'
-import { hospitalAdmin, superAdmin } from '../../router'
+import { superAdmin } from '../../router'
 
 export const AppRouter:FC = () => {
     return (
         <Switch>
-            {(localStorage.getItem('role') === '1' ? superAdmin : hospitalAdmin).map(r => <Route key={r.path} path={r.path} component={r.component} exact={r.exact}/>)}
+            { superAdmin.map(r => <Route key={r.path} path={r.path} component={r.component} exact={r.exact}/>)}
             <Redirect to='/'/>
         </Switch>
     )
